7970ae430cf7725f7e94687391079012124c30cd
Afonso Machado <afonsosmachado@gmail.com>
2022-11-25 18:41:05 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="flex items-center">
+    <main class="w-full h-screen overflow-x-hidden">
+      <HelloWorld :msg="' Yeah'" />
+    </main>
+  </div>
+</template>
